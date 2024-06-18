ae71aba0c606788f350b71978ee8c5c35ec6a6dd
ShreyasiDT <shreyasit1792@gmail.com>
2024-06-18 14:54:13 +0530

    entire project

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -28,7 +28,7 @@ export const Navbar = () => {
             <a href="#about">About</a>
           </li>
           <li>
-            <a href="#skills">Skills</a>
+            <a href="#experience">Skills</a>
           </li>
           <li>
             <a href="#projects">Projects</a>
